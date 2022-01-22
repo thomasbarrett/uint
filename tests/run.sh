e478b39727861cb78631bac9d41c7903148f605c
Thomas Barrett <tbarrett@Thomass-Mac-mini.local>
2022-01-21 21:34:45 -0800

    modify test script

--- tests/run.sh
+++ tests/run.sh
@@ -5,7 +5,7 @@ do
     if [ -f $test ] 
     then
         output="$($test 2>&1)"
-        if [ $? == 0 ]
+        if [ $? -eq "0" ]
         then
             echo "\033[0;32m[ PASS ]\033[0m $test"
         else
@@ -18,7 +18,7 @@ do
     fi
 done
 echo ""
-if [ $failed == 0 ]
+if [ $failed -eq "0" ]
 then
     echo "\033[0;32mFailed $failed Tests\033[0m"
 else
